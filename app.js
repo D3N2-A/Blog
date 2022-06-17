2f15140d787ec84d78e63651ffb7e4cffcc58a40
D3N2-A <anmolsharma999937@gmail.com>
2022-06-17 09:18:58 +0530

    home setup

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. ";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. ";
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home");
+  res.render("home", { homeContent: homeStartingContent });
 });
 
 app.listen(process.env.PORT || 3000, function () {
