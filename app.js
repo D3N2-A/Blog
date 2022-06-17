ccb2473557cfe4ab5dce3a957c5e50f676ee2079
D3N2-A <anmolsharma999937@gmail.com>
2022-06-17 10:57:44 +0530

    footer

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const bodyParser = require("body-parser");
 const app = express();
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare.";
-const aboutContent =
+const aboutStartContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. ";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. ";
+const contactStartContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. ";
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +17,17 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("home", { homeContent: homeStartingContent });
 });
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutStartContent });
+});
 
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactStartContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started at port 3000");
 });
