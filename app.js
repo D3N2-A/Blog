8e1a686f5264592e973293b700a58c477af7e678
D3N2-A <anmolsharma999937@gmail.com>
2022-06-17 08:16:26 +0530

    page setup

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser, urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started at port 3000");
